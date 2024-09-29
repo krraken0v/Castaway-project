c72578bdf00c3dc71fb9a624255b0248973b0fb8
last-commit <kohanickola@gmail.com>
2024-09-29 13:51:50 +0200

    second-commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded",function(){
+    const submitButton = document.querySelector(".loginSubmit");
+    const inputName = document.querySelector(".loginName");
+    const modalHeader = document.querySelector(".modal_header");
+    const modalButtonClose = document.querySelector(".buttonModalClose");
+    const modal = document.querySelector(".modal");
+    const inputEmail = document.querySelector(".loginEmail");
+    const modalEmail = document.querySelector(".modal__email");
+    
+    submitButton.addEventListener("click",function(){
+        modal.classList.add("modalShow");
+        const inputValueName = inputName.value;
+        const inputValueEmail = inputEmail.value;
+        modalHeader.innerText = `Thank you! ${inputValueName}`;
+        modalEmail.innerText = `Your E-mail is: ${inputValueEmail}`;
+
+    })
+    modalButtonClose.addEventListener("click",function(){
+        modal.classList.remove("modalShow");
+    })
+})
